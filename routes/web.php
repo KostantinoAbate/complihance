7dc3ea756f41265c781e585b31b302f7500b3a2c
KostantinoAbate <48536150+KostantinoAbate@users.noreply.github.com>
2026-07-06 11:40:45 +0000

    Fix styling

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use KostantinoAbate\Complihance\Http\Controllers\ConsentController;
 
 Route::prefix(config('complihance.routes.prefix', 'complihance'))
     ->middleware(config('complihance.routes.middleware', ['web']))
